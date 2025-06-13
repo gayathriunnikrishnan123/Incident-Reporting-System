f8dfa4a6108a6c17d10a9acc3bd77a9a39739b23
Sivakumar S <siva.lmcst22@gmail.com>
2025-06-13 12:07:05 +0530

    added dasboard. integrated userprofile, login, masterdata to the dasboard need to resolve login page alignment issue and password toggling

--- incident_reporting/accounts/urls.py
+++ incident_reporting/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import dashboardView, createUserView, editUserView, userListView, deleteUserView
+from accounts.views import dashboardView, createUserView, editUserView, userListView, deleteUserView, systemConfigView, userManagementView
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.forms import CustomEmailLoginForm
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="registration/login.html",authentication_form=CustomEmailLoginForm),name='login'),
     path("logout/", LogoutView.as_view(next_page="home"), name='logout'),
     path('dashboard/', dashboardView, name='dashboard'),
+    path('systemconfig/', systemConfigView, name='system-config'),
+    path('userManagement/', userManagementView, name='user-management'),
     path('create-user/',createUserView,name='create-user'),
     path('edit-user/<int:userId>/',editUserView,name='edit-user'),
     path('delete-user/<int:userId>/',deleteUserView,name='delete-user'),
