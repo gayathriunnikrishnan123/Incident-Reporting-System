fc807bcde2e40ba2ea285e4aa1d6ac3285725c30
Sivakumar S <siva.lmcst22@gmail.com>
2025-06-15 18:17:42 +0530

    password flow integrated

--- incident_reporting/accounts/urls.py
+++ incident_reporting/accounts/urls.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 from accounts.forms import CustomEmailLoginForm
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="registration/login.html",authentication_form=CustomEmailLoginForm),name='login'),
-    path("logout/", LogoutView.as_view(next_page="home"), name='logout'),
+    path('logout/', LogoutView.as_view(next_page="home"), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html',email_template_name='registration/password_reset_email.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+
+
 
     path('dashboard/', views.dashboardView, name='dashboard'),
     path('systemconfig/', views.systemConfigView, name='system-config'),
@@ -28,4 +37,5 @@ urlpatterns = [
     path('mappings/',views.departmentProfileView,name='show-maps'),
     path('edit-map/<int:mapId>/',views.departmentProfileEditView,name='edit-map'),
     path('delete-map/<int:mapId>/',views.departmentProfileDeleteView,name='delete-map'),
+
 ]
