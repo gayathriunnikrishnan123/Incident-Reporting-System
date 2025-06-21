d129ff203295e8bd963c57048f24fc41e2c41fcf
Sivakumar S <siva.lmcst22@gmail.com>
2025-06-21 13:01:40 +0530

    tested and integrated user views for incidents

--- incident_reporting/accounts/views.py
+++ incident_reporting/accounts/views.py
@@ -12,6 +12,7 @@ from accounts.models import CustomUserProfile, Role, AuditLog, DepartmentProfile
 from accounts.decorators import audit_trail_decorator, role_level_required
 from masterdata.models import Department
 from django.http import JsonResponse
+from incidents.models import Incident
 
 
 # Create your views here.
@@ -317,7 +318,7 @@ def departmentProfileDeleteView(request, mapId):
 
 # for auto populating departments from division
 
-
+@audit_trail_decorator
 def get_departments_by_division(request):
     division_id = request.GET.get("division_id")
     departments = Department.objects.filter(division_id=division_id, is_deleted=False).values("id", "name")
@@ -328,3 +329,30 @@ def get_departments_by_division(request):
 
 
 
+@login_required
+@audit_trail_decorator
+@role_level_required(1)
+def get_all_incidents(request):
+    role = request.session.get('role_name', None)
+    allIncidents=Incident.objects.all()  
+    return render(request,"all_user_incidents.html",{'allIncidents':allIncidents})
+
+@login_required
+@audit_trail_decorator
+@role_level_required(3)
+def get_my_incidents(request):
+    role = request.session.get('role_name', None)
+    allIncidents=Incident.objects.filter(assigned_to=request.user)  
+    return render(request,"my_incidents.html",{'allIncidents':allIncidents})
+
+
+
+@login_required
+@audit_trail_decorator
+@role_level_required(3)
+def incident_details_by_token(request,token):
+    incident_details=get_object_or_404(Incident, incident_token=token, is_deleted=False)
+    attachments=incident_details.attachments.all()
+    for i in attachments:
+        print(i)
+    return render(request,"user_incident_details.html",{'incident_details':incident_details,'attachments':attachments})
