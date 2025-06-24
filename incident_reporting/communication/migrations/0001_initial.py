802626ae4b7424da4730c429221f79b966080924
Alida Thomas <alidathomas@Alidas-MacBook-Air.local>
2025-06-24 13:08:02 +0530

    all solved

--- incident_reporting/communication/migrations/0001_initial.py
+++ incident_reporting/communication/migrations/0001_initial.py
@@ -10,8 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('incidents', '0002_incident_is_deleted_incidentattachment'),
+        ('incidents', '0001_initial')
     ]
 
     operations = [
