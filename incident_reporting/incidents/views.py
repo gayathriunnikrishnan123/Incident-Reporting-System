6c39177db3ca54b39deb5205abafb919697082a1
AlidaAnna <alidaannathomas.com>
2025-07-03 16:35:49 +0530

    email sending to assigned person when a incident is reported

--- incident_reporting/incidents/views.py
+++ incident_reporting/incidents/views.py
@@ -105,7 +105,55 @@ def submit_incident(request):
                     )
                 except Exception as e:
                     print("Failed to send email:", e)
-                    
+            subject = f"[New Incident Submitted] Token: {incident.incident_token}"
+            message = f"A new incident has been submitted.\n\nTitle: {incident.title}\nDivision: {incident.division}\nDepartment: {incident.department}\nToken: {incident.incident_token}"
+
+            notification_recipients = []   
+
+# 1. Assigned Responder
+            if incident.department:
+                responder_profiles = DepartmentProfile.objects.filter(
+                    department=incident.department,
+                    role__name="Responder",
+                    is_active=True,
+                    is_deleted=False
+                ).select_related('user')
+
+                notification_recipients += [responder.user.email for responder in responder_profiles]
+
+
+# 2. Assigned Reviewer (if no responder)
+            if not notification_recipients and incident.division:
+                reviewer_profiles = DepartmentProfile.objects.filter(
+                    division=incident.division,
+                    department__isnull=True,
+                    role__name="Reviewer",
+                    is_active=True,
+                    is_deleted=False
+                ).select_related('user')
+
+                notification_recipients += [reviewer.user.email for reviewer in reviewer_profiles]
+# 3. Admin (fallback)
+            if not notification_recipients:
+                admin_profiles = CustomUserProfile.objects.filter(
+                    is_superuser=True,
+                    is_active=True
+                )
+                notification_recipients += [admin.email for admin in admin_profiles]
+
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=notification_recipients,
+                    fail_silently=False
+                )
+            except Exception as e:
+                print("Failed to send email to team:", e)
+
+
+
             for key, value in request.POST.items():
                 if key.startswith('question_'):
                     question_id = key.split('_')[1]
