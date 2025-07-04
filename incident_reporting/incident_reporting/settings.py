4f9726577016993a4114dc810f90d1be0a84290e
Sivakumar S <siva.lmcst22@gmail.com>
2025-07-04 13:35:50 +0530

    updated some settings

--- incident_reporting/incident_reporting/settings.py
+++ incident_reporting/incident_reporting/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'use your mail id'
+EMAIL_HOST_USER = 'use your mail id to send'
 EMAIL_HOST_PASSWORD = 'generate your password'
 
 
